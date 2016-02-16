d0708c4e229cf8296746bafe07906dcbdfe91934
Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>
2016-02-17 00:55:09 +0100

    Problema del verano. Alogrítmicamente resuelto.
    Deberíamos buscar el orden del algoritmo. Porque creo que es
    bueno... LOF <3

---
+++ Problemas/Verano/Verano.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+public class Verano {
+
+    public static void main(String[] args) {
+	int line = 0;
+        Double menor;
+	boolean habemusMCD = false;
+	int MCD = 1;
+	try {
+	    BufferedReader br = 
+		new BufferedReader(new InputStreamReader(System.in));
+	    
+	    for (String x = br.readLine(); x != null; x = br.readLine()){
+		MCD = 1;
+		habemusMCD = false;
+		String [] mesas = x.split(" ");
+		menor = Double.parseDouble(mesas[0]);
+		for (int i = 1; i < mesas.length-1; i++) {
+		    if (menor > Double.parseDouble(mesas[i]))
+			menor = Double.parseDouble(mesas[i]);
+		}
+		for (int i = 1; i < menor && !habemusMCD; i++) {
+		    habemusMCD = (menor%(menor/i) == 0);
+		    for (int j = 0; j < mesas.length-1 && habemusMCD; j++) {
+			habemusMCD = habemusMCD && (Double.parseDouble(mesas[j])%(menor/i) == 0);
+		    }
+		    if (habemusMCD)
+			MCD = (int) (menor/i);
+		}
+		System.out.println(MCD);
+
+		line += 1;
+	    }
+	}catch (IOException e) {
+	    System.out.println("Error " + e.getMessage());
+	    e.printStackTrace();
+	}
+    }
+}
